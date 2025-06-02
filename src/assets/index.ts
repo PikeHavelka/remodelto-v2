343b6fbd0a90161542bd2de402711367705fbed7
Pavel Havelka <havelka.pvl@seznam.cz>
2025-06-02 19:52:59 +0200

    BG images in header completed.

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,25 @@
-export { default as header_bg_white_building } from "./images/header_bg_white_building.jpg"
-export { default as header_bg_white_buildings } from "./images/header_bg_white_buildings.jpg"
-export { default as header_bg_glass_building } from "./images/header_bg_glass_building.jpg"
+// HEADER
+// PC QHD (2560x1440)
+export { default as pc_qhd1_2560x1440 } from "./images/header/pc_qhd/pc_qhd1_2560x1440.jpg";
+export { default as pc_qhd2_2560x1440 } from "./images/header/pc_qhd/pc_qhd2_2560x1440.jpg";
+export { default as pc_qhd3_2560x1440 } from "./images/header/pc_qhd/pc_qhd3_2560x1440.jpg";
+
+// PC Full HD
+export { default as pc_full_hd1_1920x1080 } from "./images/header/pc_full_hd/pc_full_hd1_1920x1080.jpg";
+export { default as pc_full_hd2_1920x1080 } from "./images/header/pc_full_hd/pc_full_hd2_1920x1080.jpg";
+export { default as pc_full_hd3_1920x1080 } from "./images/header/pc_full_hd/pc_full_hd3_1920x1080.jpg";
+
+// Tablet
+export { default as tablet1_1280x853 } from "./images/header/tablet/tablet1_1280x853.jpg";
+export { default as tablet2_1280x853 } from "./images/header/tablet/tablet2_1280x853.jpg";
+export { default as tablet3_1280x853 } from "./images/header/tablet/tablet3_1280x853.jpg";
+
+// Mobile MEDIUM
+export { default as mobile_medium1_768x1152 } from "./images/header/mobile_medium/mobile_medium1_768x1152.jpg";
+export { default as mobile_medium2_768x1152 } from "./images/header/mobile_medium/mobile_medium2_768x1152.jpg";
+export { default as mobile_medium3_768x1152 } from "./images/header/mobile_medium/mobile_medium3_768x1152.jpg";
+
+// Mobile SMALL
+export { default as mobile_small1_480x720 } from "./images/header/mobile_small/mobile_small1_480x720.jpg";
+export { default as mobile_small2_480x720 } from "./images/header/mobile_small/mobile_small2_480x720.jpg";
+export { default as mobile_small3_480x720 } from "./images/header/mobile_small/mobile_small3_480x720.jpg";
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,15 +1,57 @@
 import {
-  header_bg_white_building,
-  header_bg_white_buildings,
-  header_bg_glass_building,
+  pc_qhd1_2560x1440,
+  pc_qhd2_2560x1440,
+  pc_qhd3_2560x1440,
+  pc_full_hd1_1920x1080,
+  pc_full_hd2_1920x1080,
+  pc_full_hd3_1920x1080,
+  tablet1_1280x853,
+  tablet2_1280x853,
+  tablet3_1280x853,
+  mobile_medium1_768x1152,
+  mobile_medium2_768x1152,
+  mobile_medium3_768x1152,
+  mobile_small1_480x720,
+  mobile_small2_480x720,
+  mobile_small3_480x720,
 } from "../../assets/index";
 import { useState, useEffect } from "react";
 
-const headerBGImages = [
-  header_bg_white_building,
-  header_bg_white_buildings,
-  header_bg_glass_building,
+// The arrays must have the same length! Images for srcSet.
+const qhd_imgs = [
+  pc_qhd1_2560x1440,
+  pc_qhd2_2560x1440,
+  pc_qhd3_2560x1440
 ];
+
+const full_hd_imgs = [
+  pc_full_hd1_1920x1080,
+  pc_full_hd2_1920x1080,
+  pc_full_hd3_1920x1080,
+];
+
+const tablet_imgs = [
+  tablet1_1280x853,
+  tablet2_1280x853,
+  tablet3_1280x853
+];
+
+const mobile_md_imgs = [
+  mobile_medium1_768x1152,
+  mobile_medium2_768x1152,
+  mobile_medium3_768x1152,
+];
+
+const mobile_sm_imgs = [
+  mobile_small1_480x720,
+  mobile_small2_480x720,
+  mobile_small3_480x720,
+];
+// ***********************************
+
+const TIMEOUT_DURATION = 18000;
+const INTERVAL_DURATION = 20000;
+const FADE_DURATION = "2000ms";
 
 const Header = () => {
   const [index, setIndex] = useState(0);
@@ -18,17 +60,17 @@ const Header = () => {
   useEffect(() => {
     const timeoutID = setTimeout(() => {
       setFade(false);
-    }, 18000);
+    }, TIMEOUT_DURATION);
 
     const intervalID = setInterval(() => {
       setFade(true);
 
       setTimeout(() => {
         setFade(false);
-      }, 18000);
+      }, TIMEOUT_DURATION);
 
-      setIndex((prev) => (prev + 1) % headerBGImages.length);
-    }, 20000);
+      setIndex((prev) => (prev + 1) % qhd_imgs.length);
+    }, INTERVAL_DURATION);
 
     return () => {
       clearInterval(intervalID);
@@ -38,17 +80,28 @@ const Header = () => {
 
   return (
     <header className="relative flex justify-center items-center h-screen-supp">
-
-      <div
-        style={{ backgroundImage: `url(${headerBGImages[index]})` }}
-        className={`absolute w-full h-full bg-no-repeat bg-cover bg-center transition-opacity duration-[2000ms] ease-in-out ${
+      <img
+        style={{ transitionDuration: FADE_DURATION }}
+        className={`absolute w-full h-full object-cover object-center transition-opacity ease-in-out ${
           fade ? "opacity-100" : "opacity-0"
         }`}
-      ></div>
+        src={full_hd_imgs[index]}
+        srcSet={`
+        ${mobile_sm_imgs[index]} 480w,
+        ${mobile_md_imgs[index]} 768w,
+        ${tablet_imgs[index]} 1280w,
+        ${full_hd_imgs[index]} 1920w,
+        ${qhd_imgs[index]} 2560w
+        `}
+        sizes="100vw"
+        alt="Decorative Background"
+      />
 
-      <div className="text-center text-black z-10">
-        <h1 className="uppercase">stavby & rekonstrukce</h1>
-        <p className="lowercase">
+      <div className="text-center text-gray-700 z-10">
+        <h1 className="uppercase bg-white border-2 inline-block px-4 py-1  mb-1 text-3xl">
+          stavby & rekonstrukce
+        </h1>
+        <p className="lowercase bg-white px-4 py-1 border-2 text-2xl">
           Stavební firma RemodelTO, stavba podle vašich představ
         </p>
       </div>
